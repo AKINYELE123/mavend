1689d58ecd8abd4937f8edaeb6deb1cc87d5ba8c
AKINYELE123 <boluwatifeakinyele1@gmail.com>
2025-01-29 12:36:47 +0100

    bug fixed

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextIput, FlatList, StyleSheet, TextInput, TouchableOpacity, Switch } from 'react-native'
-import {AntDesign} from 'react-native-vector-icons'
+import { View, Text, FlatList, StyleSheet, TextInput, TouchableOpacity, Switch } from 'react-native'
+import  AntDesign  from 'react-native-vector-icons/AntDesign'
 
 
 const App = () => {
@@ -39,7 +39,7 @@ const App = () => {
   }
 
   const toggleTask = (id) => {
-    setTasks(tasks.map(task = task.id === id ? { ...task, completed: !task.completed } : task))
+    setTasks(tasks.map(task => task.id === id ? { ...task, completed: !task.completed } : task));
   }
 
   const filteredTasks = tasks.filter(task =>
@@ -50,42 +50,47 @@ const App = () => {
 
 
   return (
-    <View style={[styles.container]}>
+    <View style={[styles.container, darkMode && styles.darkContainer]}>
 
-      <Switch value={darkMode} onValueChange={() => setDarkMode(!darkMode)} />
+      <View style={styles.switchContainer}>
+        <Text>{!darkMode ? "Dark Mode": "Light Mode"}</Text>
+        <Switch value={darkMode} onValueChange={() => setDarkMode(!darkMode)} />
+      </View>
       <TextInput
-        style={[styles.input]}
+        style={[styles.input, darkMode && styles.darkInput]}
         placeholder='Enter task'
         value={taskText}
         onChangeText={setTasktext}
       />
 
       <TouchableOpacity onPress={addTask} style={[styles.addButton]}>
-        <Text style>Add</Text>
+        <Text style={darkMode ? styles.darkAddText : styles.lightAddText}>Add</Text>
       </TouchableOpacity>
 
       <View style={styles.filterContainer}>
-        {['All', 'Completed','Incomplete'].map(f => (
+        {['All', 'Completed', 'Incomplete'].map(f => (
           <TouchableOpacity onPress={() => setFilter(f)} key={f}>
-            <Text style={[styles.filterText, filter === f && styles.activeFilter]}>{f}</Text>
+            <Text style={[styles.filterText, filter === f && styles.activeFilter,  darkMode && styles.darkText,]}>{f}</Text>
           </TouchableOpacity>
         ))}
       </View>
 
       <FlatList
-      data={filteredTasks}
-      keyExtractor={item => item.id.stringify()}
-      renderItem={({item}) => (
-        <View style={[styles.taskContainer]}>
-          <TouchableOpacity onPress={() => toggleTask(item.id)}>
-            <AntDesign name={item.completed ? "checkcircle" : "checkcircleo"} size={25}/>
-          </TouchableOpacity>
-          <Text style={[styles.taskText, item.completed && styles.completedTask]}>{item.text}</Text>
-          <TouchableOpacity onPress={() => deleteTask(item.id)}>
-            <AntDesign name='delete' color="red" size={25}/>
-          </TouchableOpacity>
-        </View>
-      )}
+        data={filteredTasks}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <>
+            <View style={[styles.taskContainer]}>
+              <TouchableOpacity onPress={() => toggleTask(item.id)}>
+                <AntDesign name={item.completed ? "checkcircle" : "checkcircleo"} size={24} color='green'/>
+              </TouchableOpacity>
+              <Text style={[styles.taskText, item.completed && styles.completedTask, darkMode && styles.darkText,]}>{item.text}</Text>
+              <TouchableOpacity onPress={() => deleteTask(item.id)}>
+                <AntDesign name='delete' color="red" size={24} />
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
       />
     </View>
   )
@@ -93,16 +98,21 @@ const App = () => {
 }
 
 const styles = StyleSheet.create({
+  switchContainer: {flexDirection: "row", justifyContent: "space-between", alignItems: "center"},
   container: { flex: 1, padding: 20, backgroundColor: "#fff", paddingTop: 100 },
   darkContainer: { backgroundColor: "#333" },
   input: { borderWidth: 1, padding: 10, marginVertical: 10, borderRadius: 10, },
+  darkInput: { backgroundColor: '#555', color: '#fff' },
   addButton: { backgroundColor: "blue", padding: 10, borderRadius: 10, alignItems: "center" },
-  taskContainer: {flexDirection: "row", alignItems: "center", justifyContent: "space-between", padding: 10, borderBottomWidth: 1}, 
-  taskText: {fontSize: 18},
-  completedTask: {textDecorationLine: "line-through", color: "gray"},
-  filterContainer: {flexDirection: "row", justifyContent: "space-around", marginVertical: 10},
-  filterText: {fontSize: 16, color: "gray"},
-  activeFilter: {color: 'blue', fontWeight: "Bold"}
+  darkAddText: {color: "white"},
+  lightAddText: {color: "white"},
+  taskContainer: { flexDirection: "row", alignItems: "center", justifyContent: "space-between", padding: 10, borderBottomWidth: 1 },
+  taskText: { fontSize: 18  },
+  completedTask: { textDecorationLine: "line-through", color: "gray" },
+  filterContainer: { flexDirection: "row", justifyContent: "space-around", marginVertical: 10 },
+  filterText: { fontSize: 16, color: "gray" },
+  activeFilter: { color: 'blue', fontWeight: "Bold" },
+  darkText: { color: 'white' },
 })
 
 
